d72349d1587fbc4a6877b12c59d8c7bb838fa0c0
jasjakub1 <jasjakub1@gmail.com>
2026-02-28 14:54:08 +1100

    animations mk 1

--- Scripts/playerMovement.gd
+++ Scripts/playerMovement.gd
@@ -6,7 +6,7 @@ const MAX_SPEED = 50.0
 const JUMP_VELOCITY = 250.0
 
 var SPEED = 0
-
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,13 +14,23 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = -1 * JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+		animated_sprite.play("running")
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		animated_sprite.play("running")
+	else:
+		animated_sprite.play("idle")
+	
 	if direction:
 		SPEED = clamp(SPEED + direction * ACCELERATION, -1 * MAX_SPEED, MAX_SPEED)
 	else:
